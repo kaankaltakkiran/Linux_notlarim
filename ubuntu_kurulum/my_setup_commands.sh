6b5acfbefa2a85c928b91feedf5984166f635bc8
Kaan Kaltakkıran <98158194+kaankaltakkiran@users.noreply.github.com>
2024-05-10 00:12:28 +0300

    Update my_setup_commands.sh

--- ubuntu_kurulum/my_setup_commands.sh
+++ ubuntu_kurulum/my_setup_commands.sh
@@ -34,7 +34,7 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 sudo apt update -y
 
 ## Spotify,Anydesk,SublimeMerge,Vscode yükle
-sudo apt install spotify-client anydesk sublime-merge code  -y
+sudo apt install || true  spotify-client anydesk sublime-merge code  -y
 
 #### Fare için ayarlar
 ##### Dock ünitesinde program simgesine tıklayınca küçült/büyült
