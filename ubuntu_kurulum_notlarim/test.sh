6c3699d16277dde3a92590b3a5298e6237d206b5
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2024-10-05 17:45:51 +0300

    try

--- ubuntu_kurulum_notlarim/test.sh
+++ ubuntu_kurulum_notlarim/test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Easy Screenshoot kısayolu ekleme
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom0/name 'Easy Screenshoot'
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom0/command "sh -c 'gnome-screenshot -af /home/$USER/Pictures/Screenshots/\$(date \"+%Y.%m.%d-%H.%M.%S\").png'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom0/binding '<Primary><Shift>s'
-
-# Guake Terminal kısayolu ekleme (F12)
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom1/name 'Guake Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom1/command 'guake-toggle'
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings/custom1/binding 'F12'
-
-# Mevcut kısayollara ekleme
+# Mevcut kısayolları al
 existing_bindings=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
-new_bindings="['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
-# Eğer başka kısayollar varsa onları koruyarak ekleme yap
-if [[ $existing_bindings != "@as []" ]]; then
-    new_bindings=$(echo $existing_bindings | sed "s/]/, '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']/")
+# Eğer varsayılan değilse, mevcut kısayolları işleyin
+if [[ $existing_bindings == "@as []" ]]; then
+    new_bindings="['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+else
+    # Mevcut olan kısayolların sonuna ekleme yap
+    new_bindings=$(echo $existing_bindings | sed "s/]$/, '\/org\/gnome\/settings-daemon\/plugins\/media-keys\/custom-keybindings\/custom0\/', '\/org\/gnome\/settings-daemon\/plugins\/media-keys\/custom-keybindings\/custom1\/']/")
 fi
 
+# Easy Screenshoot kısayolu ekleme
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Easy Screenshoot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "sh -c 'gnome-screenshot -af /home/$USER/Pictures/Screenshots/\$(date \"+%Y.%m.%d-%H.%M.%S\").png'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary><Shift>s'
+
+# Guake Terminal kısayolu ekleme (F12)
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Guake Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'guake-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding 'F12'
+
+# Yeni kısayolları ayarla
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$new_bindings"
 
 echo "Kısayollar başarıyla eklendi!"
